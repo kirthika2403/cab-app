de94dc89ffd6684efb5c96fd05c0f4fd0dac83e3
kirthika2403 <79511897+kirthika2403@users.noreply.github.com>
2021-04-21 17:29:26 +0530

    Update Ola.java

--- Ola.java
+++ Ola.java
@@ -9,7 +9,7 @@ public class Ola {
 		System.out.println("1.micro 2.mini 3.prime");
 	}
 
-	public static void gst(int amnt, int peak) {
+	public static void extra_charge(int amnt, int peak) {
 		double val;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("GST: 7%");
@@ -69,15 +69,18 @@ public class Ola {
 		}
 		return hour;
 	}
-
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
+	public static void login()
+	{   Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your 10 digit mobile number:");
 		long mobile = sc.nextLong();
-		String mobilenum = String.valueOf(mobile);
-		if (mobilenum.length() != 10) {
+		String mobilenum = String.valueOf(mobile);// valueOf()function is used to convert data type(int,long) to string
+													// type
+		if (mobilenum.length() != 10) 
+		{
 			System.out.println("Invalid mobile number");
-		} else {
+			login();
+		}
+		else {
 			System.out.println("Valid mobile number");
 		}
 		String password = "pass1234";
@@ -85,12 +88,17 @@ public class Ola {
 		String pass = sc.next();
 		if (pass.equals(password)) {
 			System.out.println("Have a Happy Journey!!!!");
-		} else {
+		} 
+		else 
+		{
 			System.out.println("Wrong Password");
+			login();
+			
 		}
 		display();
 		int type = sc.nextInt();
-		if (type == 1) {
+		if (type == 1) 
+		{
 			System.out.println("Car name:Micro");
 			System.out.println("Price per km: 10rs");
 			int peak = time();
@@ -98,8 +106,10 @@ public class Ola {
 			int km = sc.nextInt();
 			int total = km * 10;
 			System.out.println("The total amount is: " + total);
-			gst(total, peak);
-		} else if (type == 2) {
+			extra_charge(total, peak);
+		} 
+		else if (type == 2) 
+		{
 			System.out.println("Car name:Mini");
 			System.out.println("Price per km: 15rs");
 			int peak = time();
@@ -107,9 +117,11 @@ public class Ola {
 			int km = sc.nextInt();
 			int total = km * 15;
 			System.out.println("The total amount is: " + total);
-			gst(total, peak);
+			extra_charge(total, peak);
 
-		} else if (type == 3) {
+		} 
+		else if (type == 3) 
+		{
 			System.out.println("Car name:Prime");
 			System.out.println("Price per km: 20rs");
 			int peak = time();
@@ -117,11 +129,20 @@ public class Ola {
 			int km = sc.nextInt();
 			int total = km * 20;
 			System.out.println("The total amount is: " + total);
-			gst(total, peak);
-		} else {
+			extra_charge(total, peak);
+		} 
+		else
+		{
 			System.out.println("The entered car type is invalid");
 		}
-
 	}
+	
 
+
+	public static void main(String args[])
+	{
+		
+		login();
+		
+    }
 }
